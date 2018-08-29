86054ddd6de6902d27fd71808033bf02c7181e09
Justine Lebbrecht <justinelebbrecht@hotmail.fr>
2018-08-29 16:38:30 +0200

    code javaScript done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var container = document.getElementsByClassName('unactive');
 var source = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg", "img/img5.jpg", "img/img6.jpg", "img/img7.jpg", "img/img7.jpg", "img/img6.jpg", "img/img5.jpg", "img/img4.jpg", "img/img3.jpg", "img/img2.jpg", "img/img1.jpg"];
-
+var timeP = document.getElementsByTagName('p')[0];
+var clickTap = document.getElementsByTagName('p')[1];
 
 var historique = new Array ([]);
 var imageSelect = new Array ();
@@ -8,17 +9,37 @@ var sourceImg =  new Array ();
 var pair = new Array ([]);
 var racourcie = "nothing";
 var sourceSelect = 0;
+
+//Variable for click function
 var click = 0;
-var compt = 0;
+
+//Variable for timeFunction
+var hours = 0;
+var minutes = 0;
+var secondes = 0;
 
 var time = setInterval(timeFunction, 1000);
 
 function timeFunction() {
+  //If it's 59 secondes
+  if (secondes === 59) {
+    minutes++;
+    secondes = 0;
+  }
+  //If it's 59 minutes
+  else if (minutes === 60) {
+    hours++;
+    minutes = 0;
 
-  
-  compt++;
+  } else {
 
+    secondes++;
+
+  }
+
+  timeP.innerHTML = "Temps : " + hours + " : " + minutes + " : " + secondes;
 }
+
 //Randomize array element order in place
 //Using Durstenfeld suffle algorithme
 for (var i = source.length - 1; i > 0; i--) {
@@ -37,59 +58,75 @@ for (let i = 0; i < container.length; i++) {
    container[i].onclick = function(){
       sourceSelect= source[i];
       game(this, sourceSelect);
-      click++
+      click++;
+
+      clickTap.innerHTML = "Nombres de click : " + click;
 
       };
 }
 
 function game(image, srcSelect) {
-    //take img in a table
-     imageSelect.push(image);
+    if (image !== imageSelect[0]) {
+      //take img in a table
+       imageSelect.push(image);
 
-     //take source of img
-     image.src = srcSelect;
-     sourceImg.push(image.src);
+       //take source of img
+       image.src = srcSelect;
+       sourceImg.push(image.src);
 
-     //make image visible
-     image.style.opacity = 1;
-     //take element like in source table
-     racourcie = image.src.substr(62, 74);
-     console.log(racourcie);
-     //if 2 element a egal
-     if (imageSelect.length === 2 && sourceImg[0] === sourceImg[1]) {
-       console.log();
-       pairs();
-     } else if (imageSelect.length === 2) {
-       impairs();
-     }
+       //make image visible
+       image.style.opacity = 1;
+       //take element like in source table
+
+       racourcie = image.src.substr(62, 74);
+
+       //if 2 element a egal
+       if (imageSelect.length === 2 && sourceImg[0] === sourceImg[1]) {
+
+         pairs();
+
+       } else if (imageSelect.length === 2) {
+
+         impairs();
+
+       }
+    }
+
 
 }
 
 
 function pairs() {
 
+  //change class to pairs picture
   imageSelect[0].className = 'active';
   imageSelect[1].className = 'active';
 
+  //reset table
   imageSelect = [];
   sourceImg = [];
 }
 
 
 function impairs() {
+  //Desactivate another picture than this activate
   for (var i = 0; i < container.length; i++) {
-    if (container[i].style.opacity === "0") {
+
+    if (container[i].style.opacity === "0" || container[i].style.opacity === "") {
       container[i].style.display = "none";
     }
+
   }
 
+  //activate button and hide picture
   setTimeout(function(){
     for (var j = 0; j < container.length; j++) {
     container[j].style.opacity = "0";
     container[j].style.display = "block";
   }
+    //reset table
     sourceImg= [];
     imageSelect = [];
-  }, 1000)
+  }, 1000);
 
 }
